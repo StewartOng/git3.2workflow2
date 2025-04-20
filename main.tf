256fb4d7d72bb48467013beae25d46203051a397
stewartong <ong.hueh.chuah@gmail.com>
2025-04-20 22:54:24 +0800

    20Apr 3rd  Adding new s3 bucket

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {
     bucket = "sctp-ce9-tfstate"
     key    = "stewart-s3-tf-ci.tfstate"  # Change this if needed
-    region = "us-east-1"
+    region = "us-east-111"
   }
 }
 
