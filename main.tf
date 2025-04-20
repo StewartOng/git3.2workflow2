cc6b920eab92abc57ed02452f4d3eb1e06e5d954
stewartong <ong.hueh.chuah@gmail.com>
2025-04-20 22:23:48 +0800

    20Apr 1st Adding new s3 bucket

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "ap-southeast-1"
+  region = "us-east-1"
 }
 
 terraform {
   backend "s3" {
     bucket = "sctp-ce9-tfstate"
-    key    = "stewart-s3-tf-ci.tfstate"  #Change this
+    key    = "stewart-s3-tf-ci.tfstate"  # Change this if needed
     region = "us-east-1"
   }
 }
@@ -13,10 +13,16 @@ terraform {
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = "${split("/", "${data.aws_caller_identity.current.arn}")[1]}"
-  account_id  = "${data.aws_caller_identity.current.account_id}"
+  raw_prefix  = split("/", data.aws_caller_identity.current.arn)[1]
+  name_prefix = replace(local.raw_prefix, "_", "-")  # Replace underscore with hyphen
+  account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
   bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
+
+  tags = {
+    Name        = "Terraform S3 Bucket"
+    Environment = "Dev"
+  }
 }
